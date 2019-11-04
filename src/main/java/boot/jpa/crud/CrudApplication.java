dfde0af090f05c54f748362cf9bed6b77ba43ab5
DESKTOP-A828S0O\jiwon <C:\Users\\Documents\git_SourceTree>
2019-11-04 15:34:30 +0900

    Create domain package

--- src/main/java/boot/jpa/crud/CrudApplication.java
+++ src/main/java/boot/jpa/crud/CrudApplication.java
@@ -2,8 +2,10 @@ package boot.jpa.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CrudApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/boot/jpa/crud/domain/hero/HeroRepository.java
@@ -0,0 +1,7 @@
+package boot.jpa.crud.domain.hero;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeroRepository extends JpaRepository<Hero, Long> {
+
+}
